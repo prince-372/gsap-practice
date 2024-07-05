6dd64e20f1e3a40248c12553487763085e09e316
prince-372 <pmor18314@gmail.com>
2024-07-05 09:54:36 +0530

    first commit

---
+++ src/app/components/comman/Helper.jsx
@@ -0,0 +1,37 @@
+import notpad from "../../../../public/image/svg/notped.svg";
+import twomen from "../../../../public/image/svg/two-men.svg";
+import baar from "../../../../public/image/svg/tranding-bar.svg";
+import money from "../../../../public/image/svg/mony-bank.svg";
+import sheet from "../../../../public/image/svg/correct-sheet.svg";
+import tools from "../../../../public/image/svg/tools.svg";
+
+export const Carddata = [
+  {
+    image: notpad,
+    text: "Inspect property for storm damage or quote preparation.",
+  },
+  {
+    image: twomen,
+    text: "Meet with claims adjuster if applicable",
+  },    
+  {
+    image: baar,
+    text: "Production Planning (permitting/scheduling)",
+  },
+
+];
+export const Secondcarddata = [
+    {
+        image: money,
+        text: "Estimate repair or replacement cost",
+      },
+      {
+        image: sheet,
+        text: "Finalize necessary documentation",
+      },
+      {
+        image: tools,
+        text: "Construction begins! (Most projects completed in 1 day!)",
+      },
+
+];
